1df77901c155c85f60040309ebe3138e29b9d4dd
ismaeel <ismaeel@10.0.0.61>
2019-01-29 21:01:04 -0500

    new change

--- src/main/java/com/get/interview/model/ApplicationStatus.java
+++ src/main/java/com/get/interview/model/ApplicationStatus.java
@@ -3,12 +3,12 @@ package com.get.interview.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
-@Entity
+
 public enum ApplicationStatus {
 	
 	PENDING("Pending"),PASSED("Passed"),DISCARED("Discarded");
 
-	@Column
+
 	private String status;
 	
 	ApplicationStatus(String status){
