1389a540a05e40c09cf683e2aceb7028a7ef4f0c
fatima <fa.oujeddi@gmail.com>
2019-02-10 15:39:53 -0600

    implemented service method and added a dao method for the academic record service

--- src/main/java/com/get/interview/repository/IAcademicRecordDao.java
+++ src/main/java/com/get/interview/repository/IAcademicRecordDao.java
@@ -1,5 +1,8 @@
 package com.get.interview.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.get.interview.model.AcademicRecord;
 @Repository
 public interface IAcademicRecordDao extends CrudRepository<AcademicRecord, Long>{
 
+	@Query(value="FROM AcademicRecord WHERE resume.id =:resumeId")
+	List<AcademicRecord> findByResume(Long resumeId);
+	
 }
